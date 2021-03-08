91c4e366cef7379f45d4f7014d19c7ee489e73f9
JPC frontend <[jpcfrontend@gmail.com]>
2021-03-08 22:39:51 +0100

    test cambiar tamaño del vídeo

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,42 @@
+// DOCUMENT READY
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("tu puta madre");
+  document
+    .getElementById("increaseVideoSize")
+    .addEventListener("click", increaseVideoSize);
+});
+
+// VIDEO SIZE
+const normalWidthVideoSize = 560;
+const normalHeightVideoSize = 315;
+const maxWidthVideoSize = 1120;
+const maxHeightVideoSize = 630;
+
+const getVideoId = (btnId) => {
+  let iframeId = document.getElementById(btnId).nextElementSibling.id;
+  return iframeId;
+};
+
+const increaseVideoSize = (e) => {
+  let btnId = e.target.id;
+  let video = document.getElementById(getVideoId(btnId));
+
+  if (video.width == normalWidthVideoSize) {
+    if (document.body.clientWidth > maxWidthVideoSize) {
+      video.width = maxWidthVideoSize;
+      video.height = maxHeightVideoSize;
+      return false;
+    } else {
+      alert("pantalla demasiado pequeña para aumentar tamaño del vídeo!");
+    }
+  }
+  if (video.width == maxWidthVideoSize) {
+    video.width = normalWidthVideoSize;
+    video.height = normalHeightVideoSize;
+    return false;
+  }
+};
+
 // NAV
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
